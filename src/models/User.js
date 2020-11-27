223d521534f77104f51a36f0f637741d13ec74e3
halim <haleemyoosuph@gmail.com>
2020-11-27 01:18:31 +0100

    chore: add timestamps to user schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,43 +1,46 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: {
-        type: String,
-        unique: true
+const userSchema = mongoose.Schema(
+    {
+        firstname: String,
+        lastname: String,
+        email: {
+            type: String,
+            unique: true
+        },
+        studentMail: String,
+        password: String,
+        passwordResetToken: String,
+        passwordResetDuration: String,
+        accountVerified: {
+            type: Boolean,
+            default: false
+        },
+        accountVerificationToken: String,
+        isModerator: {
+            type: Boolean,
+            default: false
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        isStudent: {
+            type: Boolean,
+            default: false
+        },
+        school: {
+            type: mongoose.Types.ObjectId,
+            ref: 'School'
+        },
+        department: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Department'
+        },
+        yearOfEntry: String,
+        yearToGraduate: String
     },
-    studentMail: String,
-    password: String,
-    passwordResetToken: String,
-    passwordResetDuration: String,
-    accountVerified: {
-        type: Boolean,
-        default: false
-    },
-    accountVerificationToken: String,
-    isModerator: {
-        type: Boolean,
-        default: false
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    isStudent: {
-        type: Boolean,
-        default: false
-    },
-    school: {
-        type: mongoose.Types.ObjectId,
-        ref: 'School'
-    },
-    department: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Department'
-    },
-    yearOfEntry: String,
-    yearToGraduate: String
-});
+    { timestamps: true }
+);
 
 export default mongoose.model('User', userSchema);
